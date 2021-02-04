8b86c95b46c85ff4a06fbfd709192caa74e61660
rs1004 <r82.y25@gmail.com>
2021-02-04 21:56:55 +0900

    [add] yolov2

---
+++ src/model/__init__.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+
+
+class Conv2dBNLeaky(nn.Module):
+    def __init__(self, c_in, c_out, kernel_size, stride, padding=0):
+        super(Conv2dBNLeaky, self).__init__()
+
+        self.conv = nn.Conv2d(c_in, c_out, kernel_size, stride, padding, bias=False)
+        self.bn = nn.BatchNorm2d(c_out)
+        self.activation = nn.LeakyReLU(0.1)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        out = self.activation(x)
+        return out
+
+
+class Route(nn.Module):
+    def __init__(self, layers):
+        super(Route, self).__init__()
+        self.layers = layers
+
+    def forward(self, xs):
+        return torch.cat(xs, axis=1)
+
+
+class Reorg(nn.Module):
+    def __init__(self, stride):
+        super(Reorg, self).__init__()
+        self.stride = stride
+
+    def forward(self, x):
+        b, c, h, w = x.data.size()
+        ws = self.stride
+        hs = self.stride
+        x = x.view(b, c, int(h / hs), hs, int(w / ws), ws).transpose(3, 4).contiguous()
+        x = x.view(b, c, int(h / hs * w / ws), hs * ws).transpose(2, 3).contiguous()
+        x = x.view(b, c, hs * ws, int(h / hs), int(w / ws)).transpose(1, 2).contiguous()
+        x = x.view(b, hs * ws * c, int(h / hs), int(w / ws))
+        return x
---
+++ src/model/yolov2.py
@@ -0,0 +1,74 @@
+import torch.nn as nn
+from __init__ import Conv2dBNLeaky, Route, Reorg
+
+
+class YoloV2(nn.Module):
+    def __init__(self):
+        super(YoloV2, self).__init__()
+
+        self.features = nn.Sequential(
+            Conv2dBNLeaky(c_in=3, c_out=32, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+
+            Conv2dBNLeaky(c_in=32, c_out=64, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+
+            Conv2dBNLeaky(c_in=64, c_out=128, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=128, c_out=64, kernel_size=1, stride=1),
+            Conv2dBNLeaky(c_in=64, c_out=128, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+
+            Conv2dBNLeaky(c_in=128, c_out=256, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=256, c_out=128, kernel_size=1, stride=1),
+            Conv2dBNLeaky(c_in=128, c_out=256, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+
+            Conv2dBNLeaky(c_in=256, c_out=512, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=512, c_out=256, kernel_size=1, stride=1),
+            Conv2dBNLeaky(c_in=256, c_out=512, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=512, c_out=256, kernel_size=1, stride=1),
+            Conv2dBNLeaky(c_in=256, c_out=512, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0),
+
+            Conv2dBNLeaky(c_in=512, c_out=1024, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=1024, c_out=512, kernel_size=1, stride=1),
+            Conv2dBNLeaky(c_in=512, c_out=1024, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=1024, c_out=512, kernel_size=1, stride=1),
+            Conv2dBNLeaky(c_in=512, c_out=1024, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=1024, c_out=1024, kernel_size=3, stride=1, padding=1),
+            Conv2dBNLeaky(c_in=1024, c_out=1024, kernel_size=3, stride=1, padding=1),
+
+            Route(layers=[-9]),
+            Conv2dBNLeaky(c_in=512, c_out=64, kernel_size=1, stride=1),
+            Reorg(stride=2),
+
+            Route(layers=[-1, -4]),
+            Conv2dBNLeaky(c_in=1280, c_out=1024, kernel_size=3, stride=1, padding=1)
+        )
+        self.detector = nn.Conv2d(in_channels=1024, out_channels=125, kernel_size=1, stride=1)
+
+        self.route_queue = {}
+        for i, m in enumerate(self.features):
+            if isinstance(m, Route):
+                for lnum in m.layers:
+                    self.route_queue[lnum + i] = None
+
+    def forward(self, x):
+        for i, m in enumerate(self.features):
+            if i in self.route_queue:
+                self.route_queue[i] = m(x)
+            if isinstance(m, Route):
+                xs = [self.route_queue.pop(lnum + i) for lnum in m.layers]
+                x = m(xs)
+            else:
+                x = m(x)
+        out = self.detector(x)
+        return out
+
+
+if __name__ == '__main__':
+    import torch
+    model = YoloV2()
+    print(model)
+    x = torch.rand((2, 3, 416, 416))
+    print(model(x))
