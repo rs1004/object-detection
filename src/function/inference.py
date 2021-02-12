0379d625a5a7c76affe0e4439da65ca25fc8bb4a
rs1004 <r82.y25@gmail.com>
2021-02-12 11:08:54 +0900

    [fix] multiprocess

--- src/function/inference.py
+++ src/function/inference.py
@@ -3,6 +3,8 @@ import seaborn as sns
 from torchvision.ops import box_convert, batched_nms
 from PIL import Image, ImageFont, ImageDraw
 from pathlib import Path
+from functools import partial
+from multiprocessing import Pool
 from tqdm import tqdm
 
 
@@ -14,10 +16,9 @@ class Inference:
         self.__dict__.update(cfg)
 
     def run(self):
-        font = ImageFont.truetype(self.font_name, self.font_size)
         colors = [tuple([int(i * 255) for i in c]) for c in sns.color_palette('hls', n_colors=len(self.dataloader.dataset.labels))]
-        result_dir = Path(self.result_dir) / 'inference'
-        result_dir.mkdir(exist_ok=True, parents=True)
+        save_dir = Path(self.result_dir) / 'inference'
+        save_dir.mkdir(exist_ok=True, parents=True)
         num_output = min(self.num_output, len(self.dataloader))
 
         device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -32,39 +33,49 @@ class Inference:
                 # forward
                 outputs = self.model(images)
 
-                # restore
+                # draw bbox and save image
                 images = images.to('cpu')
                 outputs = outputs.to('cpu')
-                for b in range(len(outputs)):
-                    image = Image.fromarray((images[b].permute(1, 2, 0).numpy() * 255).astype('uint8'))
 
-                    boxes = box_convert(outputs[b, :, :4], in_fmt='cxcywh', out_fmt='xyxy')
-                    confs = outputs[b, :, 4]
-                    class_ids = outputs[b, :, 5:].max(dim=-1).indices
+                f = partial(self._draw_and_save, colors=colors, save_dir=save_dir)
+                with Pool() as p:
+                    saved = p.starmap(f, zip(range(count, len(images) + count), images, outputs))
 
-                    # leave valid bboxes
-                    boxes = boxes[confs > self.conf_thresh]
-                    class_ids = class_ids[confs > self.conf_thresh]
-                    confs = confs[confs > self.conf_thresh]
+                count += sum(saved)
 
-                    ids = batched_nms(boxes, confs, class_ids, iou_threshold=self.iou_thresh)
-                    boxes = boxes[ids]
-                    confs = confs[ids]
-                    class_ids = class_ids[ids]
+                if count >= num_output:
+                    break
 
-                    # draw & save
-                    draw = ImageDraw.Draw(image)
-                    for box, conf, class_id in zip(boxes, confs, class_ids):
-                        xmin, ymin, xmax, ymax = box.data.numpy() * 32
-                        color = colors[class_id.data]
-                        text = f'{self.dataloader.dataset.labels[class_id.data]}: {round(float(conf.data), 3)}'
+        print('Finished Inference')
 
-                        draw.rectangle((xmin, ymin, xmax, ymax), outline=color)
-                        draw.text((xmin, ymin), text, fill=color, font=font)
+    def _draw_and_save(self, i, image, output, colors, save_dir):
+        image = Image.fromarray((image.permute(1, 2, 0).numpy() * 255).astype('uint8'))
 
-                    image.save(result_dir / f'{count:05}.png')
-                    count += 1
+        boxes = box_convert(output[:, :4], in_fmt='cxcywh', out_fmt='xyxy')
+        confs = output[:, 4]
+        class_ids = output[:, 5:].max(dim=-1).indices
 
-                    if count > num_output:
-                        print('Finished Inference')
-                        return
+        # leave valid bboxes
+        boxes = boxes[confs > self.conf_thresh]
+        class_ids = class_ids[confs > self.conf_thresh]
+        confs = confs[confs > self.conf_thresh]
+
+        ids = batched_nms(boxes, confs, class_ids, iou_threshold=self.iou_thresh)
+        boxes = boxes[ids]
+        confs = confs[ids]
+        class_ids = class_ids[ids]
+
+        # draw & save
+        draw = ImageDraw.Draw(image)
+        font = ImageFont.truetype(self.font_name, self.font_size)
+        for box, conf, class_id in zip(boxes, confs, class_ids):
+            xmin, ymin, xmax, ymax = box.data.numpy() * 32
+            color = colors[class_id.data]
+            text = f'{self.dataloader.dataset.labels[class_id.data]}: {round(float(conf.data), 3)}'
+
+            draw.rectangle((xmin, ymin, xmax, ymax), outline=color)
+            draw.text((xmin, ymin), text, fill=color, font=font)
+
+        image.save(save_dir / f'{i:05}.png')
+
+        return 1
